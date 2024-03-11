37ac53aa9d6e9071255aaff19f1a0dec147f0d8d
Elina-dc <elina.hakobyan@doublecoconut.com>
2024-03-11 13:20:24 +0400

    Animating objects

--- src/init.js
+++ src/init.js
@@ -9,17 +9,27 @@ const init = () => {
 
 	const scene = new THREE.Scene();
 	const canvas = document.querySelector('.canvas');
+
 	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 	scene.add(camera);
 
 	const controls = new OrbitControls(camera, canvas);
 	controls.enableDamping = true;
 
-	const renderer = new THREE.WebGLRenderer({ canvas });
+	const renderer = new THREE.WebGLRenderer({
+		canvas,
+	});
 	renderer.setSize(sizes.width, sizes.height);
 	renderer.render(scene, camera);
 
-	return { sizes, scene, canvas, camera, renderer, controls };
+	return {
+		sizes,
+		scene,
+		canvas,
+		camera,
+		renderer,
+		controls,
+	};
 };
 
 export default init;
